41447cd5eef90b08342ab53d2aafe2aa8215a9e4
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-01-16 18:34:10 +0200

    add modified files

--- React-pratice/src/App.jsx
+++ React-pratice/src/App.jsx
@@ -4,8 +4,12 @@ import ContactContainer from "./ContactContainer";
 import Header from "./Header";
 
 function App() {
+  const x = ["ali", "ahmed", "ezzat"];
+  const y = x.map((name) => <h1>{name}</h1>);
+  console.log(y);
   return (
     <>
+      {y}
       <Header />
       <ContactContainer />
     </>
--- React-pratice/src/Contact.jsx
+++ React-pratice/src/Contact.jsx
@@ -1,8 +1,9 @@
 import "./Contact.css";
+import image from "./assets/picofme.png";
 const Contact = () => {
   return (
     <article className="contact-card">
-      <img src="/src/assets/picofme.png" alt="self picture" />
+      <img src={image} alt="self picture" />
       <h3>Walid Ezzat</h3>
       <div className="info-group">
         <img src="/src/assets/globe.png" alt="globe icon" />
