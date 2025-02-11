33b274165c848123228009b89be9a8be61bdce18
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-02-11 16:59:58 +0200

    11/2/2025 React proctice init

--- React-pratice/src/App.jsx
+++ React-pratice/src/App.jsx
@@ -1,42 +1,54 @@
 import React from "react";
+// import Contact from "./Contact";
+import Product from "./Product";
 
 export default function App() {
-  const [myFavoriteThings, setMyFavoriteThings] = React.useState([]);
-
-  const allFavoriteThings = [
-    "💦🌹",
-    "😺",
-    "💡🫖",
-    "🔥🧤",
-    "🟤🎁",
-    "🐴",
-    "🍎🥧",
-    "🚪🔔",
-    "🛷🔔",
-    "🥩🍝",
-    "💦🌹",
-    "😺",
-    "💡🫖",
-    "🔥🧤",
-    "🟤🎁",
-    "🐴",
-    "🍎🥧",
-    "🚪🔔",
-    "🛷🔔",
-    "🥩🍝",
-  ];
-  const thingsElements = myFavoriteThings.map((thing) => (
-    <p key={thing}>{thing}</p>
-  ));
-
-  function addFavoriteThing() {
-    setMyFavoriteThings((prev) => [...prev, allFavoriteThings[prev.length]]);
-  }
+  
 
   return (
-    <main>
-      <button onClick={addFavoriteThing}>Add item</button>
-      <section aria-live="polite">{thingsElements}</section>
-    </main>
+    
   );
 }
+
+
+
+
+
+
+// const [myFavoriteThings, setMyFavoriteThings] = React.useState([]);
+
+//   const allFavoriteThings = [
+//     "💦🌹",
+//     "😺",
+//     "💡🫖",
+//     "🔥🧤",
+//     "🟤🎁",
+//     "🐴",
+//     "🍎🥧",
+//     "🚪🔔",
+//     "🛷🔔",
+//     "🥩🍝",
+//     "💦🌹",
+//     "😺",
+//     "💡🫖",
+//     "🔥🧤",
+//     "🟤🎁",
+//     "🐴",
+//     "🍎🥧",
+//     "🚪🔔",
+//     "🛷🔔",
+//     "🥩🍝",
+//   ];
+//   const thingsElements = myFavoriteThings.map((thing) => (
+//     <p key={thing}>{thing}</p>
+//   ));
+
+//   function addFavoriteThing() {
+//     setMyFavoriteThings((prev) => [...prev, allFavoriteThings[prev.length]]);
+//   }
+
+/* <main>
+      <button onClick={addFavoriteThing}>Add item</button>
+      <section aria-live="polite">{thingsElements}</section>
+      <Contact />
+    </main> */
---
+++ React-pratice/src/Product.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Product(props) {
+  return <article>Product</article>;
+}
+
+export default Product;
