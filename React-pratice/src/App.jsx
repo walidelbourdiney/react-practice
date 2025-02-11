6e91df0eeaf042c12287e672aca8ab0c41b65fb7
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-02-11 17:45:48 +0200

    add product components

--- React-pratice/src/App.jsx
+++ React-pratice/src/App.jsx
@@ -3,17 +3,14 @@ import React from "react";
 import Product from "./Product";
 
 export default function App() {
-  
-
   return (
-    
+    <>
+      <Product name="Socks" description="Foot wear" price="25$" />
+      <Product name="Shoe" description="Foot wear" price="50$" />
+      <Product name="pants" description="body wear" price="40$" />
+    </>
   );
 }
-
-
-
-
-
 
 // const [myFavoriteThings, setMyFavoriteThings] = React.useState([]);
 
--- React-pratice/src/Product.jsx
+++ React-pratice/src/Product.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Product(props) {
-  return <article>Product</article>;
+  return (
+    <article className="product">
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+      <p>{props.price}</p>
+    </article>
+  );
 }
 
 export default Product;
